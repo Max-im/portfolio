a955b83b1996d1927359b365f5f6dfe4dec84b79
ast_mpozhydaiev <m.pozhydaiev@astoundcommerce.com>
2020-01-13 12:28:14 +0200

    make shown hoc works

--- client/src/components/hoc/Shown.js
+++ client/src/components/hoc/Shown.js
@@ -1,28 +1,49 @@
 import React from 'react';
-import store from '../../store/store';
+import { connect } from "react-redux";
 
-export default class Shown extends React.Component {
+class Shown extends React.Component {
   constructor(){
     super();
     this.shownRef = React.createRef();
   }
 
+  state = {shown: true}
+
+  componentDidMount = () => {
+    this.updateShown();
+  }
+
+  componentDidUpdate = prev => {
+    if (!this.state.shown && this.props.common.scroll !== prev.common.scroll) {
+      this.updateShown();
+    }
+  }
+
+  updateShown = () => {
+    const windowHeight = window.innerHeight;
+    const fromTop = this.shownRef.current.getBoundingClientRect().top;
+    const showElementTop = 70;
+    const fromBottom = windowHeight - fromTop - showElementTop;
+    this.setState({shown: fromBottom > 0});
+  }
+
+
   render() {
     const {component: Component, className, ...rest} = this.props;
-    const element = this.shownRef.current;
-    const { common } = store.getState();
-    const slideInAt = (common.scroll + window.innerHeight) - 300;
     const shown = {transition: 'ease-out 0.4s', position: 'relative',  transform: 'none'};
     const hidden = {transition: 'ease-out 0.4s', position: 'relative',  transform: 'translate(0px, 50px)', opacity: 0, visibility : 'collapse'};
-    let styles = shown;
-    if (element && slideInAt < element.offsetTop) {
-        styles = hidden;
-    }
-    
+    const styles = this.state.shown ? shown : hidden;
+
     return (
-      <div ref={this.shownRef} className={className} >
+      <div ref={this.shownRef} className={className} style={styles}>
         <Component {...rest}  />
       </div>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  common: state.common
+})
+
+export default connect(mapStateToProps)(Shown)
