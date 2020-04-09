77736cef47d5a89cc369f1296c9df2c71939871f
Max Pogidaev <pogidaevmo@gmail.com>
2020-04-09 17:13:35 +0300

    add helmet and log system

--- bin/dev.js
+++ bin/dev.js
@@ -1,20 +1,18 @@
 #!usr/bin/env node
 
 // eslint-disable-next-line
-require('@babel/register');
-// eslint-disable-next-line
-require("dotenv").config({ path: "./server/config/variables.env" });
+require("@babel/register");
 
 // eslint-disable-next-line
-const cors = require('cors');
-const app = require('../server');
+const cors = require("cors");
+const app = require("../server");
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 // Handle 404
-app.get('*', (req, res) => res.status(404).send());
+app.get("*", (req, res) => res.status(404).send());
 
 const port = process.env.PORT || 5000;
 
 // eslint-disable-next-line no-console
-app.listen(port, () => console.info('server run'));
+app.listen(port, () => console.info("server run"));
--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
 import logger from "morgan";
+import helmet from "helmet";
+import fs from "fs";
+import path from "path";
 import routes from "./routes";
 import { errorHandler } from "./errorHandling";
 
 const app = express();
 
 // settings
-app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-logger("dev");
+if (process.env.NODE_ENV === "production") {
+  const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+    flags: "a",
+  });
+  app.use(logger("combined", { stream: logStream }));
+  app.use(helmet());
+} else {
+  app.use(logger("dev"));
+}
 
 // routes
 app.use("/", routes);
