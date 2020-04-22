83b193361fb6473a6857120ad449bce1758835a8
Maksym Pozhydaiev <pogidaevmo@gmail.com>
2020-04-22 12:36:35 +0300

    push to heroku test

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
-import { Client } from "pg";
+import { Client } from 'pg';
 
 const client = new Client({ connectionString: process.env.DB_URL });
-client.connect();
+client.connect().catch((err) => console.error('Connect Error', err));
 
 module.exports = client;
--- server/index.js
+++ server/index.js
@@ -1,29 +1,52 @@
-import express from "express";
-import bodyParser from "body-parser";
-import logger from "morgan";
-import helmet from "helmet";
-import fs from "fs";
-import path from "path";
-import routes from "./routes";
-import { errorHandler } from "./errorHandling";
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+const fs = require('fs');
+const path = require('path');
+const { errorHandler } = require('./errorHandling');
 
 const app = express();
 
-// settings
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-if (process.env.NODE_ENV === "production") {
-  const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
-    flags: "a",
+
+// production
+if (process.env.NODE_ENV === 'production') {
+  const logStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
+    flags: 'a',
   });
-  app.use(logger("combined", { stream: logStream }));
+  app.use(logger('combined', { stream: logStream }));
   app.use(helmet());
-} else {
-  app.use(logger("dev"));
+}
+// development
+else if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line
+  require('@babel/register');
+  // eslint-disable-next-line
+  require('dotenv').config({ path: 'server/config/variables.env' });
+  // eslint-disable-next-line
+  const cors = require('cors');
+  app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+  app.use(logger('dev'));
+}
+// testing
+else if (process.env.NODE_ENV === 'testing') {
+  // TODO: testing
 }
 
 // routes
-app.use("/", routes);
+app.use('/', require('./routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
+  });
+}
+
 app.use(errorHandler);
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.info('server run'));
