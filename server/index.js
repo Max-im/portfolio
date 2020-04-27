f4df4d6e99eba74a1f1fedb93d690e5b78ac2533
Maksym Pozhydaiev <pogidaevmo@gmail.com>
2020-04-27 22:10:50 +0300

    fix sending file path

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('dist/client'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
   });
 }
 
