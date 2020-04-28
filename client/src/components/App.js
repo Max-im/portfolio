623ab4bd864715d2317c9ad3d551cc486e35da10
Maksym Pozhydaiev <pogidaevmo@gmail.com>
2020-04-28 22:01:30 +0300

    change portfolio routes order

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -42,8 +42,8 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/resume" component={Resume} />
-              <Route path="/portfolio/project/:id" component={Project} />
               <Route path={['/portfolio', '/portfolio/:page']} component={Projects} />
+              <Route path="/portfolio/project/:id" component={Project} />
 
               <Route path="/logout" component={Logout} />
               <Route path="/admin/logout" component={Logout} />
